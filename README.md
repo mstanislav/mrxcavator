059846cf1cc14932d3527c2c091d2e478e66e4c2
Mark Stanislav <mark.stanislav@gmail.com>
2020-05-23 00:39:55 -0400

    Initial import of the actual program.

--- README.md
+++ README.md
@@ -1,16 +1,84 @@
-# DSU CSC-842 - mrxcavator
+# mrxcavator
 
-mrxcavator is a CLI implementation for [CRXCavator](https://crxcavator.io) being built as part of DSU's CSC-842, "Security Tool Development"
+mrxcavator is a CLI implementation for [CRXCavator](https://crxcavator.io) being built as part of DSU's CSC-842, "Security Tool Development" course.
+
+## Overview
 
 The following overview was taken from the project's [about](https://crxcavator.io/docs#/README) page:
 > CRXcavator automatically scans the entire Chrome Web Store every 3 hours and produces a quantified risk score for each Chrome Extension based on several factors. These factors include permissions, inclusion of vulnerable third party javascript libraries, weak content security policies, missing details from the Chrome Web Store description, and more. Organizations can use this tool to assess the Chrome Extensions they have installed and to move towards implementing explicit allow (whitelisting) for their organization.
 
 The development of this tool will have five "releases" that related to the course's submission deadlines. As tool releases are produced, this README.md will be updated to share some highlights of what was contributed during that release.
 
-1. Release #1 - 05/24
-2. Release #2 - 06/07
-3. Release #3 - 06/21
-4. Release #4 - 07/05
-5. Release #5 - 07/19
+1. Release 1 - 05/24
+  * Initial creation of mrxcavator, including this README file
+  * Added support to show the version of mrxcavator (-v or --version)
+  * Added support to submit an extension by id (-s or --submit)
+  * Added support to request an extension report by id (-r or --report)
+  * Validated `flake8` and `mypy` run cleanly against the application
+  * Commented functions supporting both PEP 484 and Google'd Python Style Guide
+  * Generated application documentation (docs/mrxcavator.html) using `pdoc3`
+2. Release 2 - 06/07
+3. Release 3 - 06/21
+4. Release 4 - 07/05
+5. Release 5 - 07/19
 
 In general, the tool hopes to provide an easy-to-use interface to submit extensions to CRXcavator, gather results, cache results, provide basic reporting, and likely leverage some CLI-visualization libraries to improve user experience. At this time, no public CLI tool exists for this service, so I feel that the contribution to the community is a good focus of effort.
+
+This application passes both `flake8` and `mypy` code quality analysis.
+
+## Using mrxcavator
+
+## Installation
+1. You must be running a Python release >=3.6 for application compatibility
+2. Execute `git clone https://github.com/mstanislav/mrxcavator.git` to get it
+3. Execute `cd mrxcavator` to enter the application's root folder
+4. Execute `pip3 install -r requirements.txt` to install Python dependencies
+
+### Application Help
+```
+➜  python3 mrxcavator.py
+usage: mrxcavator.py [-h] [-s id] [-r id] [-v]
+
+optional arguments:
+  -h, --help          show this help message and exit
+  -s id, --submit id  submit extension
+  -r id, --report id  extension report
+  -v, --version       show program's version number and exit
+```
+
+### Get mrxcavator's Version
+```
+➜  python3 mrxcavator.py --version
+mrxcavator v0.1
+```
+
+### Submit an Extension
+```
+➜  python3 mrxcavator.py --submit hdokiejnpimakedhajhdlcegeplioahd
+
+        You've successfully submitted hdokiejnpimakedhajhdlcegeplioahd to CRXcavator.
+```
+
+### Get an Extension Report
+```
+➜  python3 mrxcavator.py --report hdokiejnpimakedhajhdlcegeplioahd
+
+Overview
+================================================================================
+    Extension Name: LastPass: Free Password Manager
+    Extension ID:   hdokiejnpimakedhajhdlcegeplioahd
+    Newest Version: 4.18.0.4 (2018-10-09)
+    Store Rating:   4.594451 stars
+
+Risk
+================================================================================
+    CSP Policy:     86
+    RetireJS:       100
+    Web Store:      6
+
+    Permissions:
+      >Required:    110
+      >Optional:    85
+
+    ** Risk Score:  387 **
+```
---
+++ mrxcavator.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""mrxcavator.py: CLI client for CRXcavator.io"""
+
+__author__ = "Mark Stanislav"
+__license__ = "MIT"
+__version__ = "0.1"
+
+import sys
+import json
+import inspect
+import requests
+import argparse
+
+crxcavator_api_base = 'https://api.crxcavator.io/v1'
+
+
+def error(message: str, fatal=False) -> bool:
+    """Prints a passed-in message and then exits with False or a failure exit.
+
+    Args:
+        message: A message string.
+
+    Returns:
+        False or exits the application with a failure status code.
+    """
+
+    print(f"\n\t{message}\n")
+    if fatal is True:
+        sys.exit(1)
+    else:
+        return False
+
+
+def call_api(end_point: str, method: str, values=None) -> dict:
+    """Calls an API endpoint with a passed-in HTTP method and an optional dict
+    of values for APIs that required parameters to be sent in the request.
+
+    Args:
+        end_point: An API endpoint path string.
+        method: The HTTP method string to use for the API call.
+        values: An optional dict of values to pass as API parameters.
+
+    Returns:
+        A dict of API results or an empty dict.
+    """
+
+    endpoint = crxcavator_api_base + end_point
+
+    if method == 'GET':
+        response = requests.get(endpoint)
+    elif method == 'POST':
+        response = requests.post(endpoint, json=values)
+    else:
+        error(f"'{method}' is not a valid HTTP method.")
+
+    if response.status_code == 200:
+        return json.loads(response.content.decode('utf-8'))
+    elif response.status_code == 401:
+        error("401 - API Not Authorized - Please check your API token.")
+    elif response.status_code == 403:
+        error("403 - API Error - Please check your API parameters.")
+    elif response.status_code == 404:
+        error("404 - API Not Found - Please check your API endpoint.")
+    else:
+        error("An unknown API error has occurred.")
+
+    return {}
+
+
+def get_report_summary(result: dict) -> bool:
+    """Prints a formatted report of information for the given extension.
+
+    Args:
+        result: A dict of extension information.
+    Returns:
+        True.
+    """
+
+    id = result[0]['extension_id']
+    version = result[0]['version']
+
+    result = result[0]['data']
+
+    print(inspect.cleandoc(f"""
+            \t
+            Overview
+            {'='*80}
+            \tExtension Name:\t{result['webstore']['name']}
+            \tExtension ID:\t{id}
+            \tNewest Version:\t{version} ({result['webstore']['last_updated']})
+            \tStore Rating:\t{result['webstore']['rating']} stars
+            \t
+            Risk
+            {'='*80}
+            \tCSP Policy:\t{result['risk']['csp']['total']}
+            \tRetireJS: \t{result['risk']['retire']['total']}
+            \tWeb Store: \t{result['risk']['webstore']['total']}
+            \t
+            \tPermissions:
+              \t  >Required:\t{result['risk']['permissions']['total']}
+              \t  >Optional:\t{result['risk']['optional_permissions']['total']}
+            \t
+            \t** Risk Score:\t{result['risk']['total']} **
+            \t
+            """))
+
+    return True
+
+
+def submit_extension(id: str) -> bool:
+    """Submits an extension (by ID) for CRXcavator to process.
+
+    Args:
+        id: An extension identifier string.
+
+    Returns:
+        A boolean.
+    """
+    result = call_api('/submit', 'POST', {'extension_id': id})
+
+    if result['code'] == 802:
+        error(f"No extension called {id} was found. Please check your ID.")
+        return False
+    else:
+        print(f"\n\tYou've successfully submitted {id} to CRXcavator.\n")
+
+    return True
+
+
+def get_report(id: str) -> bool:
+    """Requests the CRXcavator report (in JSON) for the given extension ID.
+
+    Args:
+        id: An extension identifier string.
+
+    Returns:
+        A boolean.
+    """
+    result = call_api('/report/' + id, 'GET')
+
+    if result is None:
+        error(f"No reports were found for extension {id}.")
+        return False
+    else:
+        get_report_summary(result)
+        return True
+
+
+if __name__ == "__main__":
+    if sys.version_info[0] < 3 or sys.version_info[1] < 6:
+        print("Please use Python >=3.6 with this program.\n")
+        sys.exit(1)
+    else:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-s', '--submit', metavar='id',
+                            help='submit extension')
+        parser.add_argument('-r', '--report', metavar='id',
+                            help='extension report')
+        parser.add_argument('-v', '--version', action='version',
+                            version='mrxcavator v' + __version__)
+
+        if len(sys.argv) < 2:
+            parser.print_help()
+            sys.exit(1)
+
+        args = parser.parse_args()
+
+        if args.submit:
+            submit_extension(args.submit)
+        elif args.report:
+            get_report(args.report)
